de685d3e9c998cdb9c181d9447e1815d5407d16c
lcsgborges <lcsgborges@gmail.com>
2025-10-28 20:39:24 -0300

    feat: criar teste para buscar telemetria com dados de tempo mockados

--- api/tests/test_db.py
+++ api/tests/test_db.py
@@ -4,11 +4,11 @@ import pytest
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.models import Route
+from api.models import Route, Telemetry
 
 
 @pytest.mark.asyncio
-async def test_create_route(session: AsyncSession, mock_db_time):
+async def test_create_route_db(session: AsyncSession, mock_db_time):
     with mock_db_time(model=Route) as time:
         commands = (
             'ANDAR 50 CM, GIRAR 90 GRAUS DIREITA, ANDAR 20 CM,'
@@ -29,4 +29,35 @@ async def test_create_route(session: AsyncSession, mock_db_time):
             'commands': commands,
             'created_at': time,
             'updated_at': time,
+        }
+
+
+@pytest.mark.asyncio
+async def test_create_telemetry_db(session: AsyncSession, mock_db_time):
+    with mock_db_time(model=Telemetry) as time:
+        new_telemetry = Telemetry(
+            average_speed=10,
+            distance_traveled=200,
+            energy_consumed=100,
+            average_current=100,
+            status='success',
+            route_id=1,
+        )
+
+        session.add(new_telemetry)
+        await session.commit()
+
+        telemetry = await session.scalar(
+            select(Telemetry).where(Telemetry.id == 1)
+        )
+
+        assert asdict(telemetry) == {
+            'id': 1,
+            'average_speed': 10,
+            'distance_traveled': 200,
+            'energy_consumed': 100,
+            'average_current': 100,
+            'status': 'success',
+            'route_id': 1,
+            'created_at': time,
         }
